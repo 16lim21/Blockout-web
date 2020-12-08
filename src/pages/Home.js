95e439e9f8a5c7b9cf2b7ceeee15e0c8120995b2
Michael <michael.li@columbia.edu>
2020-12-08 11:29:22 -0500

    Adding some row headers

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,6 +5,55 @@
 import React, { useState } from "react";
 import GoogleButton from "../components/GoogleButton";
 import axios from "axios";
+import styled from "styled-components";
+
+const Background = styled.div`
+    background-color: #282c34;
+    display: flex;
+    flex-direction: column;
+    min-height: 100vh;
+`
+
+const Title = styled.h1`
+    margin-top: 10px;
+    align-self: center;
+    color: white;
+`
+const ToDos = styled.div`
+    display: flex;
+    flex-direction: column;
+    padding: 10px;
+`
+const RowHeaders = styled.div`
+    display: flex;
+    flex-direction: row;
+    align-items: flex-end;
+    color: white;
+`
+
+const EventTitle = styled.div`
+    flex: 2 1 0;
+`
+
+const Duration = styled.div`
+    flex: 1 1 0;
+`
+
+const DueDate = styled.div`
+    flex: 1 1 0;
+`
+
+const Status = styled.div`
+    flex: 1 1 0;
+`
+
+const NewEvent = styled.button`
+    background: white;
+    padding: 5px 8px;
+    border: 2px solid #B9D9EB;
+    border-radius: 10px;
+    cursor: pointer;
+`
 
 const Home = () => {
     const [events, setEvents] = useState([]);
@@ -24,9 +73,9 @@ const Home = () => {
     };
 
     return (
-        <div>
-            <h1>Home Page</h1>
-            <button onClick={getData}>Click Me</button>
+        <Background>
+            <Title>Blockout</Title>
+            {/* <button onClick={getData}>Click Me</button> */}
             <div>
                 {events.map((event) => (
                     <div key={event.id}>
@@ -34,8 +83,17 @@ const Home = () => {
                     </div>
                 ))}
             </div>
+            <ToDos>
+                <RowHeaders>
+                    <EventTitle></EventTitle>
+                    <Duration>Duration</Duration>
+                    <DueDate>Due Date</DueDate>
+                    <Status>Status</Status>
+                    <NewEvent>New ToDo</NewEvent>
+                </RowHeaders>
+            </ToDos>
             <GoogleButton />
-        </div>
+        </Background>
     );
 };
 
